18b82b6af7e67faf5be8897b4007218e70c5fbc1
Timur Sharpov <57627185+Sharpovich@users.noreply.github.com>
2020-07-17 16:38:51 +0300

    Update iterator_script.py

--- iterator_script.py
+++ iterator_script.py
@@ -17,3 +17,33 @@ class RandomIterator:
 
 for i in RandomIterator(10):
     print(i)
+
+    
+    x=[-2,-1,0,1,2]
+y=[]
+for i in x:
+    y.append(i*i)
+print(y)
+
+x=[-2,-1,0,1,2]
+y=[i*i for i in x if i>0]
+print(y)
+
+z=[(x,y) for x in range(3) for y in range(3) if y>=x]
+print(z)
+
+z=[]
+for x in range(3):
+    for y in range(3):
+        if y>=x:
+            z.append((x,y))
+print(z)
+
+a = [i + 1 for i in range(4)]
+print(a)
+a = [i for i in range(4)]
+print(a)
+a = [i for i in range(5)][1:]
+print(a)
+a = list(i + 1 for i in range(4))
+print(a)
